2970745cfa223d587a2870a15288977033808d4f
Cristian Bitca <cristianbitca@cristians-air.home>
2022-06-23 21:37:52 +0100

    JavaDateAndTime

--- src/Introduction/JavaCurrencyFormatter.java
+++ src/Introduction/JavaCurrencyFormatter.java
@@ -1,2 +1,30 @@
-package Introduction;public class JavaCurrencyFormatter {
+package Introduction;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaCurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double payment = inp.nextDouble();
+        inp.close();
+        Locale USLocale = new Locale("en", "US");
+        Locale INLocale = new Locale("en", "IN");
+        Locale CNLocale = new Locale("zh", "CN");
+        Locale FRLocale = new Locale("fr", "FR");
+
+        if (payment > 0 && payment < Math.pow(10,9)){
+            NumberFormat currencyFormatterUS = NumberFormat.getCurrencyInstance(USLocale);
+            System.out.println("US: " + currencyFormatterUS.format(payment));
+            NumberFormat currencyFormatterIN = NumberFormat.getCurrencyInstance(INLocale);
+            System.out.println("India: " + currencyFormatterIN.format(payment));
+            NumberFormat currencyFormatterCH = NumberFormat.getCurrencyInstance(CNLocale);
+            System.out.println("China: " + currencyFormatterCH.format(payment));
+            NumberFormat currencyFormatterFR = NumberFormat.getCurrencyInstance(FRLocale);
+            System.out.println("France: " + currencyFormatterFR.format(payment));
+        }else
+            System.out.println("Wrong amount.");
+
+    }
 }
--- src/Introduction/JavaDateAndTime.java
+++ src/Introduction/JavaDateAndTime.java
@@ -1,2 +1,56 @@
-package Introduction;public class JavaDateAndTime {
-}
+/*
+You are given a date. You just need to write the method, FindDay, which returns the day on that date.
+ */
+package Introduction;
+
+
+import java.io.*;
+
+import java.time.LocalDate;
+
+    class Result {
+
+        /*
+         * Complete the 'findDay' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts following parameters:
+         *  1. INTEGER month
+         *  2. INTEGER day
+         *  3. INTEGER year
+         */
+
+        public static String findDay(int month, int day, int year) {
+            LocalDate localDate = LocalDate.of(year,month,day);
+            java.time.DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+
+            String theDay = dayOfWeek.toString();
+            return theDay;
+        }
+
+    }
+
+    public class JavaDateAndTime {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int month = Integer.parseInt(firstMultipleInput[0]);
+
+            int day = Integer.parseInt(firstMultipleInput[1]);
+
+            int year = Integer.parseInt(firstMultipleInput[2]);
+
+            Result.findDay(month, day, year);
+
+            bufferedWriter.newLine();
+
+            bufferedReader.close();
+            bufferedWriter.close();
+        }
+    }
+
+
+
